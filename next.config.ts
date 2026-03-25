8037bffe1e5128a88cd8bbf2d28d82360963fcdb
Roberto Perez <robertoisworking@gmail.com>
2026-03-24 22:49:19 -0300

    changed path to images

--- next.config.ts
+++ next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
     },
     images: { remotePatterns: [
             { protocol: 'https', hostname: 'covers.openlibrary.org' },
-            { protocol: 'https', hostname: 'lspfdyhgsrgsxcju.public.blob.vercel-storage.com' },
+            { protocol: 'https', hostname: '7wazoxqr2h0x0ktx.public.blob.vercel-storage.com' },
         ]}
 };
 
